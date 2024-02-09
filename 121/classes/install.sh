5df8f4181c6bb7f0d6a767deafdcd74aed51648d
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2024-02-08 16:00:04 -0800

    add install script

---
+++ 121/classes/install.sh
@@ -0,0 +1,3 @@
+curl https://raw.githubusercontent.com/jjoeldaniel/si/main/121/classes/classes.cpp -o classes.cpp
+curl https://raw.githubusercontent.com/jjoeldaniel/si/main/121/classes/run.sh -o run.sh
+chmod +x run.sh
--- 121/classes/run.sh
+++ 121/classes/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-clang++ "${1}" --std=c++17
+clang++ classes.cpp --std=c++17
 ./a.out
 rm a.out
