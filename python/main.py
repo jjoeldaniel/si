a49b931e99ee133a2cd827f05a113cba3a8c88fe
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2024-04-11 17:04:09 -0700

    add python ex

---
+++ python/main.py
@@ -0,0 +1,14 @@
+print("hi!")
+
+name = "Joel"
+age = 23
+is_si = True
+
+print(name, age, is_si)
+
+print("hello, " + name + ".")
+print(f"hello, {name}! You are {age} years old!!!")
+print(4 % 3)
+
+if 1 + 1 == 2 or (2 + 2 == 4 and 5 + 5 == 10):
+    print("duh")
