71854958602c2cb028f098395043d593c891a9e2
Joel Daniel Rico <100006388+jjoeldaniel@users.noreply.github.com>
2025-10-14 10:48:43 -0700

    fix

--- 121/composition/car.hpp
+++ 121/composition/car.hpp
@@ -1,64 +1,72 @@
-#ifndef CAR_HPP
-#define CAR_HPP
-
 #include <iostream>
 #include <string>
+#include <utility>
 
 /**
  * @class Engine
- * @brief Represents the engine component of a car. This class is complete.
+ * @brief Represents the engine of a car.
+ *
+ * This class is fully implemented for you. It serves as the component
+ * that will be part of the Car class.
  */
 class Engine {
 private:
-    int horsepower_;
-    bool isRunning_;
+  int horsepower_;
+  bool isRunning_;
 
 public:
-    // Constructor
-    Engine(int hp) : horsepower_(hp), isRunning_(false) {}
+  // Constructor to initialize horsepower and set the engine to off.
+  Engine(int hp) {}
 
-    // Starts the engine
-    void start() {
-        if (!isRunning_) {
-            std::cout << "Engine starts. Vroom vroom!" << std::endl;
-            isRunning_ = true;
-        }
-    }
+  // Starts the engine
+  void start() {}
 
-    // Stops the engine
-    void stop() {
-        if (isRunning_) {
-            std::cout << "Engine stops." << std::endl;
-            isRunning_ = false;
-        }
-    }
+  // Stops the engine
+  void stop() {}
 
-    // Getters
-    bool getIsRunning() const { return isRunning_; }
-    int getHorsepower() const { return horsepower_; }
+  // Getter for the running state
+  bool getIsRunning() const { return isRunning_; }
+
+  // Getter for horsepower
+  int getHorsepower() const { return horsepower_; }
 };
-
 
 /**
  * @class Car
- * @brief Represents a car that HAS-A Engine (composition).
- * Your task is to implement the methods of this class in Car.cpp.
+ * @brief Represents a car that contains an Engine.
+ *
+ * This is the class you need to complete. A Car "has-a" Engine. This is a
+ * key example of composition in OOP.
  */
 class Car {
 private:
-    std::string make_;
-    std::string model_;
-    Engine engine_; // The Car contains an Engine object
+  std::string make_;
+  std::string model_;
+  Engine engine_; // The Car HAS-A Engine object as a member variable
 
 public:
-    // Constructor declaration
-    Car(std::string make, std::string model, int engineHp);
+  // TODO: PROBLEM 1 - Implement the Constructor
+  // The Car's constructor needs to initialize its own members (`make_`,
+  // `model_`) AND the `engine_` member. The best way to do this is with a
+  // member initializer list. The `engine_` needs its horsepower value.
+  Car(std::string make, std::string model, int engineHp) {
+    // The body can be empty because the initializer list does all the work!
+  }
 
-    // Method declarations for actions the car can perform
-    void startCar();
-    void stopCar();
-    void drive();
-    void displayInfo() const;
+  // TODO: PROBLEM 2 - Delegate to the Engine
+  // This method should start the car by starting its engine.
+  void startCar() {}
+
+  // TODO: PROBLEM 3 - Delegate to the Engine
+  // This method should stop the car by stopping its engine.
+  void stopCar() {}
+
+  // TODO: PROBLEM 4 - Use the Engine's State
+  // A car can only drive if its engine is running.
+  void drive() {
+    // Check the state of the `engine_` component to decide what the car does.
+  }
+
+  // Displays information about the car and its engine
+  void displayInfo() const {}
 };
-
-#endif // CAR_HPP
